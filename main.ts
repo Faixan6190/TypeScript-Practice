4d42a26bff3c965b6deff5c1d44dd9016e83aa9f
Faixan6190 <faixan6190@gmail.com>
2024-03-29 01:27:34 +0500

    Unary Operator

--- main.ts
+++ main.ts
@@ -14,3 +14,64 @@
 
 // let colorFavorite = "Blue";
 // console.log(colorFavorite);
+
+// let userName: string = "Faizan";
+// userName = "Faizan";
+
+// let a: string = "Pakistan";
+// let b: number = 9;
+// let c: boolean = true;
+
+//  let firstName: string = "Muhammad";
+//  let lastName: string = "Faizan"
+//  let fullName: string = firstName + " " + lastName;
+//  console.log(fullName)
+//  let middleName: string = "Ahmed";
+//  console.log(middleName);
+
+// let nameFirst: string = "Muhammad";
+// let nameMiddle: string = "Faizan";
+// let nameLast: string = "Ahmed";
+
+// let nameFull: string = `${nameFirst} ${nameMiddle} ${nameLast}`;
+// console.log(nameFull);
+
+// let num1: number = 2;
+// let num2: number = 5;
+
+// let cart: number = num1 + num2;
+// console.log(cart);
+
+// let num1: number = 2;
+// let num2: number = 5;
+// let cart: number = num1 - num2;
+// console.log(cart);
+
+// let num1: number = 2;
+// let num2: number = 5;
+// let cart: number = num1 * num2;
+// console.log(cart);
+
+// let num1: number = 10;
+// let num2: number = 2;
+// let cart: number = num1 / num2;
+// console.log(cart);
+
+// let layer: number = 5;
+// let apple: number = 5;
+// let power: number = layer ** 2;
+// console.log(power);
+
+// console.log(5 % 2);
+
+// let a = 5;
+// let b = 4;
+
+// console.log(a++);
+// console.log(++b);
+
+let a = 5;
+let b = 2;
+
+console.log(a--);
+console.log(--b);
