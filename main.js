29e8067610eb1a8757f59ba20d2817e2a3ff43af
Faixan6190 <faixan6190@gmail.com>
2024-03-29 00:57:48 +0500

    Variable

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// console.log("Hello World");
+// const Header = () => {
+//   console.log("Hello World");
+// };
+// let favoriteColor = "Red";
+// favoriteColor = "Blue";
+// console.log(favoriteColor);
+var favoriteColor = "Red";
+console.log(favoriteColor);
+var colorFavorite = "Blue";
+console.log(colorFavorite);
--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-const Header = () => {
-  console.log("Hello World");
-};
+// const Header = () => {
+//   console.log("Hello World");
+// };
+
+// let favoriteColor = "Red";
+// favoriteColor = "Blue";
+
+// console.log(favoriteColor);
+
+// let favoriteColor = "Red";
+// console.log(favoriteColor);
+
+// let colorFavorite = "Blue";
+// console.log(colorFavorite);
