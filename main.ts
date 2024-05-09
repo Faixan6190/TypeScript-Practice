663e159f889f315b1e728dd9612bc876db411511
Faixan6190 <faixan6190@gmail.com>
2024-05-09 14:19:13 +0500

    fix error

--- main.ts
+++ main.ts
@@ -155,16 +155,16 @@
 
 // myName();
 
-const stdObj = {
-  name: "Faizan",
-  id: 1,
-  email: "faizan@gmail.com",
-};
+// const stdObj = {
+//   name: "Faizan",
+//   id: 1,
+//   email: "faizan@gmail.com",
+// };
 
-const tempObj = {
-  name: "Yahoo",
-};
+// const tempObj = {
+//   name: "Yahoo",
+// };
 
-const tempObjStd = {
-  name: "Yahoo",
-};
+// const tempObjStd = {
+//   name: "Yahoo",
+// };
