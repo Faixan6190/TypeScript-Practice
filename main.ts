3931deb247d60307a1bfa69bff792ac14e04b8ca
Faixan6190 <faixan6190@gmail.com>
2024-06-09 23:43:04 +0500

    learning

--- main.ts
+++ main.ts
@@ -245,8 +245,8 @@
 //   console.log("wear sunglass");
 // }
 
-let stdObj = {
-  name: "faizan",
-  id: "1",
-};
+// let stdObj = {
+//   name: "faizan",
+//   id: "1",
+// };
 // console.log("stdObj", stdObj);
