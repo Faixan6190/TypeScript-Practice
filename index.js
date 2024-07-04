2717bfc298a97df45f12a9c93747fdfbc35a3316
Faixan6190 <faixan6190@gmail.com>
2024-07-04 15:58:37 +0500

    add import file

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 let b = "Hello Duniya";
-exports.default = b;
+export default b;
--- main.js
+++ main.js
@@ -1,4 +1,319 @@
-"use strict";
 // console.log("Hello World");
-Object.defineProperty(exports, "__esModule", { value: true });
+// const Header = () => {
+//   console.log("Hello World");
+// };
+// let favoriteColor = "Red";
+// favoriteColor = "Blue";
+// console.log(favoriteColor);
+// let favoriteColor = "Red";
+// console.log(favoriteColor);
+// let colorFavorite = "Blue";
+// console.log(colorFavorite);
+// let userName: string = "Faizan";
+// userName = "Faizan";
+// let a: string = "Pakistan";
+// let b: number = 9;
+// let c: boolean = true;
+//  let firstName: string = "Muhammad";
+//  let lastName: string = "Faizan"
+//  let fullName: string = firstName + " " + lastName;
+//  console.log(fullName)
+//  let middleName: string = "Ahmed";
+//  console.log(middleName);
+// let nameFirst: string = "Muhammad";
+// let nameMiddle: string = "Faizan";
+// let nameLast: string = "Ahmed";
+// let nameFull: string = `${nameFirst} ${nameMiddle} ${nameLast}`;
+// console.log(nameFull);
+// let num1: number = 2;
+// let num2: number = 5;
+// let cart: number = num1 + num2;
+// console.log(cart);
+// let num1: number = 2;
+// let num2: number = 5;
+// let cart: number = num1 - num2;
+// console.log(cart);
+// let num1: number = 2;
+// let num2: number = 5;
+// let cart: number = num1 * num2;
+// console.log(cart);
+// let num1: number = 10;
+// let num2: number = 2;
+// let cart: number = num1 / num2;
+// console.log(cart);
+// let layer: number = 5;
+// let apple: number = 5;
+// let power: number = layer ** 2;
+// console.log(power);
+// console.log(5 % 2);
+// let a = 5;
+// let b = 4;
+// console.log(a++);
+// console.log(++b);
+// let a = 5;
+// let b = 2;
+// console.log(a--);
+// console.log(--b);
+// let c = 5;
+// console.log(c);
+// const b = "faizan";
+// console.log(b);
+// const Testing = () => {
+//   console.log("Hello");
+// };
+// Testing();
+// const Testing2 = () => {
+//   console.log(Testing2, "testing");
+// };
+// Testing2();
+// let user: string = "faizan";
+// console.log(user, "user");
+// const UserTesting = () => {
+//   const userName: string = "Sheryar";
+//   if (userName === "Faizan") {
+//     console.log("Welcome Faizan");
+//   } else {
+//     console.log("I Don't Recognize You....!");
+//   }
+// };
+// UserTesting();
+// let a: string = "Faizan";
+// console.info(a);
+// console.log(a);
+// const a = "faizan";
+// console.log(a);
+// let a = 5;
+// let b = 4;
+// a++;
+// ++b;
+// console.log(a);
+// console.log(b);
+// let a = 5;
+// a++;
+// console.log(a);
+// let b = 3;
+// ++b;
+// console.log(b);
+// let a: string = "Faizan";
+// console.log("a", a);
+// function myName() {
+//   console.log("Hello");
+// }
+// myName();
+// const myName = () => {
+//   console.info("myName", myName);
+// };
+// myName();
+// const myName = () => {
+//   console.log("myName", myName);
+// };
+// myName();
+// const stdObj = {
+//   name: "Faizan",
+//   id: 1,
+//   email: "faizan@gmail.com",
+// };
+// const tempObj = {
+//   name: "Yahoo",
+// };
+// const tempObjStd = {
+//   name: "Yahoo",
+// };
+// const user = true;
+// if (typeof user === "string") {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// const b :string = "24"
+// console.log(typeof b)
+// console.log(a)
+// const a = 5;
+// console.log(a);
+// const user = "fa";
+// const user = "faizan";
+// const user = faizan;
+// if (user) {
+//   console.log(user);
+// } else {
+//   console.log(user, "user");
+// }
+// let c = 10;
+// c += 5;
+// console.log(c, "c");
+// let a = 5;
+// let b = 2;
+// let c = 2;
+// let isEqual = a == b;
+// let isNotEqual = a != b;
+// let isGreaterThan = a > b;
+// let isLessThan = a < b;
+// console.log(
+//   "isEqual",
+//   isEqual,
+//   "isNotEqual",
+//   isNotEqual,
+//   "isGreaterThan",
+//   isGreaterThan,
+//   "isLessThan",
+//   isLessThan
+// );
+// let logicalAnd = a > 0 && b > 0;
+// let logicalOr = a < 0 || b > 0;
+// let logicalNot = !(a > 0);
+// let isRaining = false;
+// if (isRaining) {
+//   console.log("Wear a raincoat");
+// } else {
+//   console.log("wear sunglass");
+// }
+// let weather = "";
+// if (weather === "raining") {
+//   console.log("wear a raincoat");
+// } else if (weather === "cloudy") {
+//   console.log("wear a light jacket");
+// } else {
+//   console.log("wear sunglass");
+// }
+// let stdObj = [
+//   {
+//     id: "1",
+//     name: "Faizan",
+//     email: "Faizan@gmail.com",
+//   },
+//   {
+//     id: "2",
+//     name: "Sheikh",
+//   },
+//   {
+//     id: "3",
+//     name: "Ahmed",
+//   },
+//   {
+//     id: "4",
+//     name: "User",
+//   },
+//   {
+//     id: "5",
+//     name: "Sheryar",
+//   },
+//   {
+//     id: "6",
+//     name: "Faizan Sheikh",
+//   },
+// ];
+// console.log("stdObj", stdObj);
+// do while loop
+// let person: {
+//   name: string;
+//   age: number;
+//   home: string;
+//   currentLovedCity: string;
+//   isStatus: boolean;
+// } = {
+//   name: "faizan",
+//   age: 23,
+//   home: "a34",
+//   currentLovedCity: "Karachi",
+//   isStatus: false,
+// };
+// let arr: [string, string];
+// let a: string = "faizan";
+// let b: string = "ahmed";
+// arr.push([a, b]);
+// let arr: [string, string][] = []; // arr is an array of tuples, each tuple containing two strings
+// let a: string = "Faizan";
+// let b: string = "Sheikh";
+// arr.push([a, b]);
+// console.log("arr", arr);
+// let user = "faizan";
+// console.log("yours---->", user);
+// let dayOff = "Friday";
+// switch (dayOff) {
+//   case "Friday":
+//     console.log("Take a half break");
+//     break;
+//   case "Saturday":
+//     console.log("Go Hiking");
+//     break;
+//   case "Sunday":
+//     console.log("Read a book");
+//     break;
+//   default:
+//     console.log("Work on a hobby");
+// }
+// let answer: string = "not";
+// if (answer === "correct") {
+//   console.log("You got it right!");
+// } else {
+//   console.log("Sorry, that's not correct!");
+// }
+// let guess: number = 7;
+// let target: number = 5;
+// if (guess > target) {
+//   console.log("Your guess is too low.");
+// } else if (guess < target) {
+//   console.log("Your guess is too high.");
+// } else {
+//   console.log("You guessed correctly!");
+// }
+// let isFriend: string = "Faizan";
+// if (isFriend === "Faizan" || isFriend === "Sheikh") {
+//   console.log(`${isFriend} is your friend.`);
+// } else {
+//   console.log(`${isFriend} is not your friend`);
+// }
+// import inquirer from "inquirer";
+// let isFriend = await inquirer.prompt([
+//   {
+//     name: "name",
+//     type: "string",
+//     message: "Enter your friend name:",
+//   },
+// ]);
+// if (isFriend.name === "Faizan" || isFriend.name === "Sheikh") {
+//   console.log(`${isFriend.name} is your frined.`);
+// } else {
+//   console.log(`${isFriend.name} is not your friend.`);
+// }
+// let player1: string = "Rock";
+// let player2: string = "Scissors";
+// import inquirer from "inquirer";
+// let player1 = await inquirer.prompt([
+//   {
+//     name: "name",
+//     type: "string",
+//     message: "Enter Your Choice",
+//   },
+// ]);
+// let player2 = await inquirer.prompt([
+//   {
+//     name: "name",
+//     type: "string",
+//     message: "Enter Your Choice",
+//   },
+// ]);
+// if (player1.name === player2.name) {
+//   console.log("It's a tie!");
+// } else if (
+//   (player1.name === "Rock" && player2.name === "Scissors") ||
+//   (player1.name === "Scissors" && player2.name === "Paper") ||
+//   (player1.name === "Paper" && player2.name === "Rock")
+// ) {
+//   console.log("Player 1 wins!");
+// } else {
+//   console.log("Player 2 wins!");
+// }
+// function halfFryEgg(): number {
+//   let cooked = 1 + 1.5 + 2;
+//   return cooked;
+// }
+// let response: number = halfFryEgg();
+// console.log("response", response);
+import b from "./index";
 let a = "Hello World";
+console.log("a", a);
+console.log("b", b);
