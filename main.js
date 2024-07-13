dd72115cc6dc4f4e67e9448c75c53a71e8ee78c9
Faixan6190 <faixan6190@gmail.com>
2024-07-13 16:19:00 +0500

    problem solving

--- main.js
+++ main.js
@@ -442,7 +442,7 @@
 const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
 let text = "";
 for (let i = 0; i < cars.length; i++) {
-    text += cars[i] + "<br>";
+    text += cars[i];
 }
 console.log(text);
 export {};
--- main.ts
+++ main.ts
@@ -571,6 +571,6 @@ const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
 
 let text = "";
 for (let i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br />";
+  text += cars[i] + " ";
 }
 console.log(text);
