bde5780b550e3e7ac21a4dd2966e47c2b58c2ecd
Faixan6190 <faixan6190@gmail.com>
2024-07-16 18:08:31 +0500

    add

--- main.ts
+++ main.ts
@@ -581,8 +581,8 @@
 //   console.log("Hello World");
 // }
 
-let i = 0;
-while (i < 5) {
-  console.log("Hello");
-  i++;
-}
+// let i = 0;
+// while (i < 5) {
+//   console.log("Hello");
+//   i++;
+// }
