f9717777bb58515d676a2b56438f3068d0ee7afa
Faixan6190 <faixan6190@gmail.com>
2024-07-24 02:07:29 +0500

    type inference

--- index.ts
+++ index.ts
@@ -5,7 +5,19 @@
 // // userName = 24;
 
 //DATA TYPES//
-let userName: string = "Faizan Ahmed";
-let age: number = 23;
-let isStudent: boolean = false;
-let randomValue: any = "http://www.google.com";
+// let userName: string = "Faizan Ahmed";
+// let age: number = 23;
+// let isStudent: boolean = false;
+// let randomValue: any = "http://www.google.com";
+
+//TYPE INFERENCE//
+
+//strongly typed syntax
+let a: string = "Pakistan";
+let b: number = 9;
+let c: boolean = true;
+
+//type inference
+let e = "USA";
+let f = 10.9;
+let g = false;
