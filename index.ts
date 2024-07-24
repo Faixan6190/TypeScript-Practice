0c2074c137711e86fb89a465e873ef3fc54ac984
Faixan6190 <faixan6190@gmail.com>
2024-07-25 01:02:19 +0500

    add currency converter

--- index.ts
+++ index.ts
@@ -59,19 +59,58 @@
 // console.log(result);
 
 //INQUIRER//
+// import inquirer from "inquirer";
+
+// const input1 = await inquirer.prompt({
+//   name: "num1",
+//   type: "number",
+//   message: "Kindly enter your first no:",
+// });
+
+// const input2 = await inquirer.prompt({
+//   name: "num2",
+//   type: "number",
+//   message: "kindly enter your second no:",
+// });
+
+// let total: number = input1.num1 + input2.num2;
+// console.log(total);
+
 import inquirer from "inquirer";
 
-const input1 = await inquirer.prompt({
-  name: "num1",
-  type: "number",
-  message: "Kindly enter your first no:",
-});
+const currency: any = {
+  USD: 1,
+  INR: 74.57,
+  EUR: 0.91,
+  DHIRAM: 3.67,
+  PKR: 278.25,
+};
 
-const input2 = await inquirer.prompt({
-  name: "num2",
-  type: "number",
-  message: "kindly enter your second no:",
-});
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter the currency you want to exchange",
+    type: "list",
+    choices: ["USD", "INR", "EUR", "DHIRAM", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter the currency in which you want to exchange",
+    type: "list",
+    choices: ["USD", "INR", "EUR", "DHIRAM", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number",
+  },
+]);
 
-let total: number = input1.num1 + input2.num2;
-console.log(total);
+let userfromcurrency = user_answer.from;
+let usertocurrency = user_answer.to;
+let amounttoconvert = currency[userfromcurrency];
+let amountinwhichconverted = currency[usertocurrency];
+let amount = user_answer.amount;
+let baseAmount = amount / amounttoconvert;
+let amountconverted = baseAmount * amountinwhichconverted;
+console.log(amountconverted);
