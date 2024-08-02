f3eabb18b21e6b2cf7c7573b22f06a95262bbb96
Faixan6190 <faixan6190@gmail.com>
2024-08-02 18:07:51 +0500

    for in loop

--- index.ts
+++ index.ts
@@ -641,7 +641,13 @@
 //   console.log(flavor);
 // }
 
-for (let i = 0; i < 5; i++) {
-  // console.log("Hello", +i);
-  console.log("Chai pila do");
+// for (let i = 0; i < 5; i++) {
+//   // console.log("Hello", +i);
+//   console.log("Chai pila do");
+// }
+
+let arr = ["Faizan", "Sheikh", "Ahmed", "Khizer"];
+
+for (let key in arr) {
+  console.log(key);
 }
